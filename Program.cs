894cb5ddd85d4609de172aa8c5343bc0665445df
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-24 16:26:08 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+using BibliotecaDeArquivosDoWindows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBibliotecaExterna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ListaDeArquivos = new GetFiles();
+
+            var ListaMeusDocumentos = ListaDeArquivos.RetornaArquivosDoMeuDocumentos();
+            imprimir(ListaMeusDocumentos);
+
+            
+            var ListaGit = ListaDeArquivos.RetornaArquivosGit();
+            if(ListaGit == null)
+            {
+                Console.WriteLine("Diretório inexis");
+            }
+            else
+            {
+                imprimir(ListaGit);
+            }
+
+            var listaImg = ListaDeArquivos.RetornaArquivosImagesFiles();
+             imprimir(listaImg);
+           // imprimir(ListaDeArquivos.RetornaArquivosImagesFiles());
+
+        }
+
+        public static void imprimir(string[] lista)
+        {
+            for (int i = 0; i < lista.Length; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+   
+
+}
+
+    
+       
+			
+
+			
+
